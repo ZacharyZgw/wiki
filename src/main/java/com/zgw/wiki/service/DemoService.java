ec95623044fb1744ef5d701f13ae4b9330ea210d
zacharyzgw <2711927321@qq.com>
2021-06-05 21:10:48 +0800

    6-5-2 新增了ebook表，并生成了mapper,domain,xml

---
+++ src/main/java/com/zgw/wiki/service/DemoService.java
@@ -0,0 +1,18 @@
+package com.zgw.wiki.service;
+
+import com.zgw.wiki.domain.Demo;
+import com.zgw.wiki.mapper.DemoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DemoService {
+    @Autowired
+    private DemoMapper demoMapper;
+
+    public List<Demo> list(){
+        return demoMapper.selectByExample(null);
+    }
+}
